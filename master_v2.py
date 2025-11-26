45f73d235b4947e5ff7139b4dc58ead3d3e4dfc4
Nilson Barboza <barbosa.nilsonneto@gmail.com>
2025-11-26 18:17:21 -0300

    STYLE: Trabalhando para afinar design frontend pagina windhtml - preparando para trabalhar a troca personalizada

--- master_v2.py
+++ master_v2.py
@@ -33,7 +33,7 @@ CONFIG = {
     'MAX_FILE_SIZE_MB': int(os.getenv('MAX_FILE_SIZE_MB', 10)),
     'MAX_IMAGE_SIZE_MB': int(os.getenv('MAX_IMAGE_SIZE_MB', 5)),
     'REQUEST_TIMEOUT': int(os.getenv('REQUEST_TIMEOUT', 10)),
-    'OUTPUT_DIR': os.getenv('OUTPUT_DIR', 'componetes'),
+    'OUTPUT_DIR': os.getenv('OUTPUT_DIR', 'htmlWind'),
 }
 
 OUT_DIR = CONFIG['OUTPUT_DIR']
